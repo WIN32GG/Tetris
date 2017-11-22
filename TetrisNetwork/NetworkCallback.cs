18da9135ac2850c96a5b1b8a191f99282d646ae9
WIN32GG <gregor.jouet@laposte.net>
2017-11-22 08:57:57 +0100

    Network basis

---
+++ TetrisNetwork/NetworkCallback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisNetwork
+{
+    public interface NetworkCallback
+    {
+        void HandlePacket1Connect(Packet1Connect p1);
+
+    }
+}
---
+++ TetrisNetwork/packets/Packet1Connect.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisNetwork
+{
+    public class Packet1Connect:Packet
+    {
+
+        private String playerName;
+        private int gameVersion;
+
+        public Packet1Connect() : base(1)
+        {
+        }
+
+        public override void ReadPacket(BinaryReader reader)
+        {
+            this.playerName = reader.ReadString();
+            this.gameVersion = reader.ReadInt32();
+        }
+
+        public override void WritePacket(BinaryWriter writer)
+        {
+            writer.Write(this.playerName);
+            writer.Write(this.gameVersion);
+        }
+    }
+}
